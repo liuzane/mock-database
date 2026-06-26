c695e6efdbb991ec06856f34ea235a7f50275804
liuzane <liu.zheng.legend@outlook.com>
2026-06-26 17:51:00 +0800

    fix: fix: set public path to '/' in production

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -2,37 +2,39 @@ import path from 'path';
 import { ModuleFederationPlugin } from '@module-federation/enhanced';
 import mfConfig from './module-federation.config.mjs';
 
-export default {
-  entry: './src/bootstrap.ts',
-  output: {
-    path: path.resolve(process.cwd(), 'dist'),
-    filename: '[name].js',
-    publicPath: '/',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new ModuleFederationPlugin(mfConfig),
-  ],
-  experiments: {
-    outputModule: true,
-  },
-  devServer: {
-    port: 1000,
-    hot: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+export default (env, argv) => {
+  return {
+    entry: './src/bootstrap.ts',
+    output: {
+      path: path.resolve(process.cwd(), 'dist'),
+      filename: '[name].js',
+      publicPath: argv.mode === 'production' ? '/mock-database/' : '/',
+      clean: true,
     },
-  },
-};
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin(mfConfig),
+    ],
+    experiments: {
+      outputModule: true,
+    },
+    devServer: {
+      port: 1000,
+      hot: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+  };
+}
